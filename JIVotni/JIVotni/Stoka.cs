7a2a28a56f204e0cb39ed9c4eb684c9a1ea0ee8c
Plaminnn <plamseizov09@gmail.com>
2025-06-15 22:51:17 +0300

    Add files via upload

---
+++ JIVotni/JIVotni/Stoka.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JIVotni
+{
+    internal class Stoka
+    {
+        private string ime;
+        private string Jivotno;
+        public string Ime { get; set; }
+        public string vid { get; set; }
+
+        public Stoka(string ime, string vid)
+        {
+            Ime = ime;
+            this.vid = vid;
+        }
+
+       
+    }
+}
